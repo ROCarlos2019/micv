be93fa1c0348398876ad8a7bec1e57c9588b0754
Pineda Tabarez <rcpineda@minsait.com>
2025-06-10 09:08:38 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mi-cv-app',
+  webDir: 'www'
+};
+
+export default config;
